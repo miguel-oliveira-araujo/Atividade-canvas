762bef428b7b2ef1fe55e3c10355137f99ebb86d
miguel-oliveira-araujo <migueloliveira.araujo@etec.sp.gov.br>
2023-05-08 12:17:30 -0300

    Botando o canva do circulo

--- AtividadeCanva/JS/script.js
+++ AtividadeCanva/JS/script.js
@@ -1,4 +1,15 @@
-const canvas = document.getElementById('canvas');
+{
+const canvas = document.getElementById('canvasq');
 const ctx = canvas.getContext('2d');
-    ctx.FillStyle ="green";
-    ctx.FillRect =(10,10,150,100);
+    ctx.fillStyle ='green';
+    ctx.fillRect(10,10,150,100);
+}
+
+{
+const canvasc =document.getElementById('canvasc');
+const ctx = canvas.getContext('2d');
+    ctx.fillStyle ='red';
+    context.beginPath();
+    context.arc(300,200,150,0,2*Math.PI);
+    context.stroke();
+}
