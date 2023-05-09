9bf0a0faadd091cc8d30fb539a2eaccbeede3ed0
miguel-oliveira-araujo <migueloliveira.araujo@etec.sp.gov.br>
2023-05-09 17:08:32 -0300

    Update script.js

--- AtividadeCanva/JS/script.js
+++ AtividadeCanva/JS/script.js
@@ -1,15 +1,14 @@
 {
-const canvas = document.getElementById('canvasq');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
     ctx.fillStyle ='green';
     ctx.fillRect(10,10,150,100);
 }
-
-{
-const canvasc =document.getElementById('canvasc');
-const ctx = canvas.getContext('2d');
-    ctx.fillStyle ='red';
-    context.beginPath();
-    context.arc(300,200,150,0,2*Math.PI);
-    context.stroke();
+    
+{let circle = palco.getContext('2d');
+circle.beginPath();
+circle.arc(225, 35, 25, 0, Math.PI * 1);
+circle.linewidth = 7;
+circle.strokestyle="black";
+circle.stroke();
 }
